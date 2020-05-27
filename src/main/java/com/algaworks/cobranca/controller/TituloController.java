04213fe69bf45000a86d3c7640d844b3c53f9618
Eliezer <eliezer1789j@gmail.com>
2020-05-27 16:18:59 +0100

    enviando os dados para o servidor

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -2,13 +2,27 @@ package com.algaworks.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
 		// the return MUST BE the NAME of the HTML file.
+		return "CadastroTitulo";
+	}
+	
+	// Os nomes dos campos no FORMULARIO devem ser os mesmos dos atributos da ENTIDADE TITULO.
+	// No HTML, a property NAME deve ter o mesmo nome do ATRIBUTO.
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		
+		System.out.println(">>>> " + titulo.getDescricao());
+		
 		return "CadastroTitulo";
 	}
 	
