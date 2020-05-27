84ce45a7d2d3aade89f49a5b161908975a5eba4b
Eliezer <eliezer1789j@gmail.com>
2020-05-27 20:07:17 +0100

    mostrando mensagem usando thymeleaf

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
@@ -24,10 +25,21 @@ public class TituloController {
 	// Os nomes dos campos no FORMULARIO devem ser os mesmos dos atributos da ENTIDADE TITULO.
 	// No HTML, a property NAME deve ter o mesmo nome do ATRIBUTO.
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		
+		// param: The name of the View that will be rendered
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		
+		// sending data to the view.
+		// here we are sending an attribute called mensagem with a message.
+		// key-value. key: mensagem, value:"Titulo salvo com sucesso!"
+		// the view can access it.
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		
+		
+		return mv;
 	}
 	
 }
