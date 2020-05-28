4c9cddf57e9e088f2025da2f6687ceb4a5a5bfc5
Eliezer <eliezer1789j@gmail.com>
2020-05-28 22:50:56 +0100

    montando o combo dinamicamente com thymeleaf

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 
@@ -17,9 +22,9 @@ public class TituloController {
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		// the return MUST BE the NAME of the HTML file.
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	// Os nomes dos campos no FORMULARIO devem ser os mesmos dos atributos da ENTIDADE TITULO.
@@ -37,9 +42,23 @@ public class TituloController {
 		// key-value. key: mensagem, value:"Titulo salvo com sucesso!"
 		// the view can access it.
 		mv.addObject("mensagem", "Titulo salvo com sucesso!");
-		
-		
+
 		return mv;
+	}
+	
+	/* this annotation define an atribute to be shared with the view.
+	 * the name of the property that the view will access will be
+	 * the name of the return type <'StatusTitulo'> concatenated with the word List.
+	 * StatusTituloList - this is the Spring default.
+	 * if we want to give a name to the property, we can specify it, like we did in here.
+	 * ("todosStatusTitulo"). So the property name in the view will be todosStatusTitulo.
+	 * Now we dont need to call 
+	 * mv.addObject("todosStatusTitulo", StatusTitulo.values());
+	   throughout the code. with this anottation, the property will be availabe always.
+	 */
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 	
 }
